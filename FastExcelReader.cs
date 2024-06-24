eb59dabf5138567bcdc73e676d62627fdb076830
barak <ostr@post.bgu.ac.il>
2024-06-24 18:27:46 +0300

    Add project files.

---
+++ FastExcelReader.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExcelReader
+{
+    public class FastExcelReader
+    {
+        string Filename, ExtractPath;
+        public  FastExcelReader(string filename)
+        {
+            Filename = filename;
+        }
+        public void ExtractDoc()
+        {
+
+        }
+
+    }
+}
